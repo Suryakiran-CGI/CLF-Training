65b1b2b2db7acda51f0346c3b89b6e42dddc3515
Suryakiran-CGI <92424705+Suryakiran-CGI@users.noreply.github.com>
2021-11-15 10:47:39 +0530

    Syntax errors resolved

--- src/Lab1_Assignments/Excersice_8.java
+++ src/Lab1_Assignments/Excersice_8.java
@@ -2,6 +2,38 @@ package Lab1_Assignments;
 
 import java.util.Scanner;
 
+
+//Way - 1
+
+import java.util.*;
+
+class Test{
+    public boolean checkNumber(int n){
+        boolean flag = Math.sqrt(n)%2==0 ? true:false;
+        return flag;
+    }
+}
+
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Test t = new Test();
+        System.out.print("Enter the number : ");
+        int num;
+        num= sc.nextInt();
+        
+        if(t.checkNumber(num)){
+            System.out.println(num+" is power of 2");
+        }else{
+            System.out.println(num+" is not power of 2");
+        }
+    }
+}
+
+
+
+//Way - 2
 class Test1{
 	public boolean CheckNumber(int n) {
 		boolean flag = false;
